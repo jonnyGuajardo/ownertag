01c47026a13a4775a9507a4c3ce9ea755e124fea
JonnyGuajardo <jonnyguajardo@gmail.com>
2026-06-04 10:04:21 -0600

    fix: corregir URL de Supabase

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://rsyhycnodwvmagmfhbpy.supabase.co/rest/v1/'
+const supabaseUrl = 'https://rsyhycnodwvmagmfhbpy.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJzeWh5Y25vZHd2bWFnbWZoYnB5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA1MzU0MjgsImV4cCI6MjA5NjExMTQyOH0.xKRCHNqqz2Fwxle0aUvfnAuNHHjboETq_ATrnsPIemU'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
